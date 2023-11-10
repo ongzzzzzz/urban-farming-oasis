cbb140a2f090f36e981df2ba5f67a6537e8fabc6
Programmer420-1 <limweixin17@gmail.com>
2023-11-11 00:08:49 +0800

    Added error handling for map

--- pages/components/scan-result.js
+++ pages/components/scan-result.js
@@ -15,7 +15,7 @@ export default function Result({ soil_type, soil_desc, crops, start_next_pred })
         <span className="my-2 w-10/12">{soil_desc}</span>
         <div className="my-2 w-10/12 text-3xl font-bold text-left">Recommended Crops:</div>
         {
-          crops.map((crop, i) => {
+          (crops||[]).map((crop, i) => {
             return <div key={i}>
               <div className="bg-white rounded-b-lg w-10/12 text-center text-2xl font-bold p-2 mt-4">{crop["crop_name"]}</div>
               <Image src={"https://source.unsplash.com/random/200x200/?" + crop["crop_name"]} alt={crop["crop_name"]}
@@ -27,7 +27,7 @@ export default function Result({ soil_type, soil_desc, crops, start_next_pred })
                 <div className="text-lg">Example crops:</div>
                 <ul className="list-disc list-inside">
                   {
-                    crop["examples"].map((example, j) => {
+                    (crop["examples"]||[]).map((example, j) => {
                       return <li key={j}>{example}</li>
                     })
                   }
